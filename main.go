a29da07493b052a82867c26156f5a89c8d22efbc
Ernesto Alejandro Nappi <eanappi@gmail.com>
2021-03-30 20:24:41 -0300

    Execute the randomuser module to testing

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/eanappi/randomuser"
+)
 
 func main() {
 	fmt.Println("Test")
+	randomuser.Test()
+	users, err := randomuser.NewRandomUserJson(10)
+	if err != nil {
+		fmt.Errorf("The service is offline by: %s", err)
+	}
+
+	fmt.Println(users)
 }
