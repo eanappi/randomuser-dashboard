4d73fcb904909c510ab7fcfa7f8a6191aa038e0c
Ernesto Alejandro Nappi <eanappi@gmail.com>
2021-03-31 14:01:07 -0300

    Rename instance function of package

--- main.go
+++ main.go
@@ -7,12 +7,12 @@ import (
 )
 
 func main() {
-	fmt.Println("Test")
-	randomuser.Test()
-	users, err := randomuser.NewRandomUserJson(10)
+	users, err := randomuser.NewUsers(10)
 	if err != nil {
 		fmt.Errorf("The service is offline by: %s", err)
 	}
 
-	fmt.Println(users)
+	user := users.Results
+
+	fmt.Println(user[0].Gender)
 }
