483a80591f5fb5ea2bf8fc183d37ce63e48c03f3
Ernesto Alejandro Nappi <eanappi@gmail.com>
2021-04-14 20:08:44 -0300

    List on template the summary content

--- main.go
+++ main.go
@@ -2,17 +2,36 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"log"
+	"net/http"
 
 	"github.com/eanappi/randomuser"
 )
 
-func main() {
+func renderDashboard(w http.ResponseWriter, r *http.Request) {
+	var data []map[string]string
+
 	users, err := randomuser.NewUsers(10)
 	if err != nil {
 		fmt.Errorf("The service is offline by: %s", err)
 	}
+	for index := range users.Results {
+		data = append(data, users.Summary(index))
+	}
+	dashboardTemplate, _ := template.ParseFiles("templates/index.tmpl")
+	err = dashboardTemplate.Execute(w, data)
+	if err != nil {
+		log.Println("Error on template :", err)
+		return
+	}
+}
 
-	for index, _ := range users.Results {
-		fmt.Println(users.Summary(index))
+func main() {
+	http.HandleFunc("/", renderDashboard)
+	err := http.ListenAndServe("0.0.0.0:8080", nil)
+	if err != nil {
+		log.Fatal("Error initializing server:", err)
+		return
 	}
 }
